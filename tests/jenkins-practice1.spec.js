a0daabb150a7b687dadc97e5aead4a95c0b29b1f
ChandeepaNimsara <chandeeparodrigo97@gmail.com>
2025-06-05 11:45:40 +0530

    jenkins-practice update9

--- tests/jenkins-practice1.spec.js
+++ tests/jenkins-practice1.spec.js
@@ -15,5 +15,6 @@ test('has title test 2', async ({ page }) => {
     await page.waitForTimeout(2200);
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/abc/)
+    
     console.log("Ending Jenkins Practice Test 2");
 });
