bf1df1d7205a7f26f1045cfff08700f569e5d806
ChandeepaNimsara <chandeeparodrigo97@gmail.com>
2025-06-16 15:19:25 +0530

    jenkins-practice update13

--- tests/jenkins-practice1.spec.js
+++ tests/jenkins-practice1.spec.js
@@ -3,7 +3,7 @@ const { test, expect, chromium } = require("@playwright/test");
 test('has title test 1', async ({ page }) => {
     console.log("Start Jenkins Practice Test 1")
     await page.goto('https://playwright.dev/');
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(2000);
     await expect(page).toHaveTitle(/Playwright/)
     console.log("Ending Jenkins Practice Test 1");
 });
@@ -11,7 +11,7 @@ test('has title test 1', async ({ page }) => {
 test('has title test 2', async ({ page }) => {
     console.log("Start Jenkins Practice Test 2")
     await page.goto('https://playwright.dev/');
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(2000);
     await expect(page).toHaveTitle(/abc/)
     console.log("Ending Jenkins Practice Test 2");
 });
